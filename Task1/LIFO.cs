fda056fc2ed0c533531178c41dbc38dd28646630
Volodymyr Rothenbert <vovkawool@outlook.com>
2021-01-09 13:44:41 +0300

    c

---
+++ Task1/LIFO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    public class LIFO
+    {
+        private int[] array;
+        private int length;
+
+        public LIFO(int size)
+        {
+            array = new int[size];
+        }
+
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public int Get()
+        {
+            lock (array)
+            {
+                var index = array.Length - length;
+                var item = array[index];
+                array[index] = 0;
+                length--;
+                return item;
+            }
+        }
+
+        public void Push(int item)
+        {
+            lock (array)
+            {
+                array[array.Length - length - 1] = item;
+                length++;
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new LIFO(10);
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() =>
+                {
+                    PushAndGet(stack);
+                });
+            }
+            Console.ReadLine();
+        }
+        private static void PushAndGet(LIFO stack)
+        {
+            for (int i = 0; i < stack.Length; i++)
+            {
+                stack.Push(i);
+                stack.Get();
+                Console.WriteLine("fdscdsvdsvsdvs");
+            }
+
+            Console.WriteLine("finish");
+        }
+    }
+}
